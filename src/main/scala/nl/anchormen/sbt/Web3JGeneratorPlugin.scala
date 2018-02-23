f529dfac0217b7db6b7bc62fde24fbc8c40f38c9
Lawrence Lee <l.lee@anchormen.nl>
2018-02-23 10:39:31 +0100

    Introduced AbiBin scala file

--- src/main/scala/nl/anchormen/sbt/Web3JGeneratorPlugin.scala
+++ src/main/scala/nl/anchormen/sbt/Web3JGeneratorPlugin.scala
@@ -1,179 +1,83 @@
 package nl.anchormen.sbt
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.web3j.codegen.SolidityFunctionWrapperGenerator
 import sbt.Keys._
-import sbt.plugins.JvmPlugin
-import sbt.{AutoPlugin, PluginTrigger, _}
+import sbt.{AutoPlugin, Def, PluginTrigger, _}
 
 object Web3JGeneratorPlugin extends AutoPlugin {
-	override def requires: JvmPlugin.type = plugins.JvmPlugin
+	override def requires: Plugins = plugins.JvmPlugin
 	override def trigger: PluginTrigger = allRequirements
 
 	object autoImport {
-		lazy val generateWrapper = TaskKey[Unit]("generate-wrapper", "Generate Java wrapper classes")
-		lazy val outputPath = SettingKey[File]("output-path", "The directory to output the generated classes")
-		lazy val packageName = SettingKey[String]("package-name", "The package name for the generated classes")
-		lazy val smartContracts = SettingKey[File]("smart-contracts", "The directory containing the smart contracts")
-		lazy val useJavaNativeTypes = SettingKey[Boolean]("use-java-native-type", "Use Java native types or Solidity types")
+		lazy val web3JGenerateWrapper = taskKey[Unit]("Generate Java wrapper classes")
+		lazy val web3JOutputPath = settingKey[File]("The directory to output the generated classes")
+		lazy val web3JSmartContracts = settingKey[File]("The directory containing the smart contracts")
+		lazy val web3JUseJavaNativeTypes = settingKey[Boolean]("Use Java native types or Solidity types")
 	}
 
 	import autoImport._
 
-	lazy val baseSettings: Seq[Setting[_]] = Seq(
-		outputPath := baseDirectory.value / "src" / "java",
-		packageName := s"${organization.value}.web3j.generated",
-		smartContracts := baseDirectory.value / "src" / "contracts",
-		useJavaNativeTypes := true,
-		generateWrapper := Generate(outputPath.value, packageName.value, smartContracts.value, useJavaNativeTypes.value)
+	lazy val baseWeb3JSettings: Seq[Setting[_]] = Seq(
+		web3JOutputPath := baseDirectory.value / "src" / "main" / "java",
+		web3JSmartContracts := baseDirectory.value / "src" / "main" / "contracts",
+		web3JUseJavaNativeTypes := true,
+		web3JGenerateWrapper := generateWrapper.value
 	)
 
-	override lazy val projectSettings: Seq[Setting[_]] = inConfig(Compile)(baseSettings)
+	override lazy val projectSettings: Seq[Setting[_]] = baseWeb3JSettings
+
+	lazy val generateWrapper: Def.Initialize[Task[Unit]] = Def.task {
+		Generate(web3JOutputPath.value, web3JSmartContracts.value, web3JUseJavaNativeTypes.value)
+	}
 }
 
-object Generate {
-	val Log: Logger = LoggerFactory.getLogger("Web3JGeneratorPlugin")
+private object Generate {
 	/**
 	  * Generate Java classes from smart contract files
 	  *
 	  * @param outputPath The path to output the generated classes
-	  * @param packageName The package name for the the generated classes
 	  * @param smartContracts Directory containing *.bin and *.abi files to be converted
 	  * @param useJavaNativeTypes Use java native types in the generated classes if set to true, use Solidity types if
 	  *                           set to false
 	  */
-	def apply(outputPath: File, packageName: String, smartContracts: File, useJavaNativeTypes: Boolean): Unit = {
-		val outputDir: File = getPackagePath(outputPath, packageName)
-		val contractFiles: Map[String, Seq[File]] = groupAndFilterContractFiles(smartContracts)
-
-		Log.debug(s"Output directory is ${outputDir.getAbsoluteFile}")
-		Log.debug(s"Processing contract files:")
-		Log.debug(s"${contractFiles.values.mkString(", ")}")
-
-		process(outputDir, packageName, contractFiles)
+	def apply(outputPath: File, smartContracts: File, useJavaNativeTypes: Boolean): Unit = {
+		val contractFiles = AbiBin.findList(smartContracts)
+		process(outputPath, contractFiles)
 	}
 
-	def process(outputDir: File, packageName: String, contractFiles: Map[String, Seq[File]]): Unit = {
-		val thread: Thread = new Thread(
-			() => {
-				Log.debug("Processing contract files")
+	/**
+	  * Process the contract
+	  *
+	  * @param outputDir
+	  * @param contractFiles
+	  */
+	def process(outputDir: File, contractFiles: List[AbiBin]): Unit = {
+				println("Processing contract files")
 
-				for (values <- contractFiles.values) {
-					val binaryFile = getBinaryFile(values)
-					val absFile = getAbsFile(values)
+				for (contract <- contractFiles) {
+						println(s"Processing ${contract.bin.toFile.getName}, ${contract.abi.toFile.getName}")
 
-					if (binaryFile.isDefined && absFile.isDefined) {
-						Log.debug(s"Processing ${binaryFile.get.getName}, ${absFile.get.getName}")
-
-						val arguments: Seq[String] = getArguments(binaryFile.get.getAbsolutePath,
-							absFile.get.getAbsolutePath,
+						val arguments: Seq[String] = getArguments(
+							contract.bin.toFile.getAbsolutePath,
+							contract.abi.toFile.getAbsolutePath,
 							outputDir.getAbsolutePath,
-							packageName)
+							contract.packageName
+						)
 
 						try {
+							println(arguments.mkString(", "))
 							SolidityFunctionWrapperGenerator.run(arguments.toArray)
 						} catch {
-							case e: Exception => Log.error(e.getMessage)
+							case e: Exception => println(e.getMessage)
 						}
-					} else {
-						Log.warn("Binary file or ABI file undefined")
-					}
+
 				}
 
-				Log.debug("Finished processing contract files")
-			}
-		)
-
-		thread.start()
-		thread.join()
+				println("Finished processing contract files")
 	}
 
 	/**
-	  * Groups binary and abs files by their filenames
-	  *
-	  * @param smartContracts The path to the directory containing the smart contracts
-	  * @return
-	  */
-	private def groupAndFilterContractFiles(smartContracts: File): Map[String, Seq[File]] = {
-		val files: Seq[File] = findContractFiles(smartContracts)
-		val fileNames: Seq[(String, File)] = filterContractFileNames(files)
-
-		fileNames.groupBy(_._1)
-				.mapValues(_.map(_._2))
-				.filter(k => k._2.lengthCompare(2) == 0)
-	}
-
-	/**
-	  * This function takes a package name such as nl.anchormen.sbt, and returns a file with path
-	  * <basePath>/nl/anchormen/sbt
-	  */
-	private def getPackagePath(basePath: File, packageName: String): File = {
-		packageName.split(".")
-				.foldLeft(basePath)((a: File, b: String) => a / b)
-	}
-
-	private def findBinaryFiles(contractPath: File): Seq[File] = {
-		val finder: PathFinder = contractPath ** "*.bin"
-		finder.get
-	}
-
-	private def getBinaryFile(contractFiles: Seq[File]): Option[File] = {
-		contractFiles.find(file => hasFileExtension(file, "bin"))
-	}
-
-	private def getAbsFile(contractFiles: Seq[File]): Option[File] = {
-		contractFiles.find(file => hasFileExtension(file, "abi"))
-	}
-
-	private def hasFileExtension(file: File, extension: String): Boolean = {
-		getFileExtension(file) match {
-			case Some(`extension`) => true
-			case _ => false
-		}
-	}
-
-	private def findAbsFiles(contractPath: File): Seq[File] = {
-		val finder: PathFinder = contractPath ** "*.abi"
-		finder.get
-	}
-
-	private def findContractFiles(contractPath: File): Seq[File] = {
-		findBinaryFiles(contractPath) ++ findAbsFiles(contractPath)
-	}
-
-	/**
-	  * Returns a list of tuples consisting of the file name without extension and the corresponding file. Only files
-	  * that have a non-empty filename (with or without extension) are returned
-	  *
-	  * @param files The input files
-	  * @return The filtered list of filenames mapped to their corresponding files
-	  */
-	private def filterContractFileNames(files: Seq[File]): Seq[(String, File)] = {
-		for {
-			file <- files
-			fileName <- getFileName(file)
-			if fileName.nonEmpty
-		} yield (fileName, file)
-	}
-
-	private def getFileName(file: File): Option[String] = {
-		val filename = file.getName
-				.split(".")
-				.dropRight(1)
-	        	.mkString(".")
-
-		Option(filename)
-	}
-
-	private def getFileExtension(file: File): Option[String] = {
-		file.getName
-				.split(".")
-				.lastOption
-	}
-
-	/**
-	  * org.web3j.codegen.SolidityFunctionWrapperGenerator /path/to/<smart-contract>.bin /path/to/<smart-contract>.abi -o /path/to/src/main/java -p com.your.organisation.name
+	  * Get arguments to supply to SolidityFunctionWrapperGenerator
 	  *
 	  * @param binaryPath
 	  * @param absPath
